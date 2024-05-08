ad379d49347e517d724dbe715f9beebff4cddd2e
OhNishant <nishant.mishra550@gmail.com>
2024-05-08 21:40:16 +0530

    responsive: steps

--- src/app/Steps.tsx
+++ src/app/Steps.tsx
@@ -1,12 +1,41 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import { useMediaQuery } from "@/hooks/use-media-query";
 import { cn } from "@/lib/utils";
 import { CircleArrowRight } from "lucide-react";
 
 const Steps = () => {
+  const isDesktop = useMediaQuery("(min-width: 768px)");
+  return isDesktop ? <StepsDesktop /> : <StepsMobile />;
+};
+const StepsMobile = () => {
+  return (
+    <div className="flex flex-col my-12 p-6">
+      <strong className="text-xl font-semibold">
+        Get personalized reccomendations with minimal effort.
+      </strong>
+      <div className="flex flex-col gap-2 py-3">
+        {steps.map((step) => {
+          return <Step key={step.count} step={step.step} count={step.count} />;
+        })}
+      </div>
+      <div className="text-xs text-[#636262]">
+        *Note: Product suggestions can be obtained without providing your mobile
+        number
+      </div>
+      <div className="flex justify-end">
+        <Button className="h-16 mt-2 rounded-none bg-[#FFD37A] hover:bg-[#FFD38A] text-black w-52 gap-2 font-normal text-base items-center">
+          Take the first step <CircleArrowRight />
+        </Button>
+      </div>
+    </div>
+  );
+};
+const StepsDesktop = () => {
   return (
     <div className="w-full items-center justify-center flex-col my-12">
       <div className="flex w-full items-center justify-center">
-        <div className="w-[1118px] bg-gray-50 flex justify-between flex-row">
+        <div className="w-[1118px] flex justify-between flex-row">
           <div>
             <div className="flex flex-col w-96 gap-8">
               <strong className="text-4xl font-semibold">
@@ -45,7 +74,9 @@ const Step = ({
 }) => {
   return (
     <div className={cn("flex flex-row gap-4 items-center", className)}>
-      <div className="rounded-full bg-[#FCF4E3] text-xl px-7 py-4">{count}</div>
+      <div className="rounded-full bg-[#FCF4E3] md:text-xl font-bold md:font-normal px-3 py-1 md:px-7 md:py-4">
+        {count}
+      </div>
       <div>{step}</div>
     </div>
   );
