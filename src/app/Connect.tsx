6fd6a557b6cb461f2773e602f3c4654ae98a8f17
OhNishant <nishant.mishra550@gmail.com>
2024-05-08 21:47:47 +0530

    yes

--- src/app/Connect.tsx
+++ src/app/Connect.tsx
@@ -2,9 +2,9 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 const Connect = () => {
   return (
-    <div className="flex my-32 justify-center bg-[#FEEFFF80] w-full items-center">
+    <div className="flex my-32 justify-center md:bg-[#FEEFFF80] bg-[#FDF8EE] md:py-0 py-2 w-full items-center">
       <div className="w-[1112px] flex gap-16">
-        <span>
+        <span className="hidden md:block">
           <Image
             src="/support_img.png"
             width={474}
@@ -12,14 +12,16 @@ const Connect = () => {
             alt="Let's talk"
           />
         </span>
-        <div className=" flex items-center justify-center mx-auto">
+        <div className=" flex md:items-center md:justify-center mx-auto">
           <div>
-            <strong className="text-4xl font-medium">
+            <strong className="md:text-4xl text-2xl font-medium">
               Still having problem choosing?
             </strong>
-            <Button className="h-16 rounded-none bg-[#F6DCF8] text-black w-40 font-normal text-base items-center">
-              Let&apos;s connect
-            </Button>
+            <div className="justify-items-end w-full">
+              <Button className="h-16 rounded-none md:bg-[#F6DCF8] bg-[#FFD37A] text-black w-40 font-normal text-base items-center">
+                Let&apos;s connect
+              </Button>
+            </div>
           </div>
         </div>
       </div>
