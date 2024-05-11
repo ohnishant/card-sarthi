f158fab0f4a1a0a362f972e80f654da5cf2b1b13
OhNishant <nishant.mishra550@gmail.com>
2024-05-12 00:12:14 +0530

    move navbar to layout
    also use turbopack for faster builds in dev

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import CardOptions from "./CardOptions";
 import Categories from "./Categories";
 import Connect from "./Connect";
 import Hero from "./Hero";
-import Navbar from "./Navbar";
 import Reviews from "./Reviews";
 import Steps from "./Steps";
 import Footer from "./Footer";
@@ -10,7 +9,6 @@ import Footer from "./Footer";
 export default function Home() {
   return (
     <>
-      <Navbar />
       <main>
         <Hero />
         <CardOptions />
