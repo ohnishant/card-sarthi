61377c9909acae65442c2f6cd5cf854991a341b5
OhNishant <nishant.mishra550@gmail.com>
2024-05-12 14:59:43 +0530

    feat: hero component props for secondary pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import CardOptions from "./_components/CardOptions";
 import Categories from "./_components/Categories";
 import Connect from "./_components/Connect";
-import Hero from "./_components/Hero";
+import Hero from "@/components/Hero";
 import Reviews from "./_components/Reviews";
 import Steps from "./_components/Steps";
-import Footer from "./_components/Footer";
 
 export default function Home() {
   return (
