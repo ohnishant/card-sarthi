1268b160d4bc87d827245c7516867c5061912f18
OhNishant <nishant.mishra550@gmail.com>
2024-05-13 03:11:34 +0530

    add: image with fallback component

---
+++ src/components/ImageWithFallback.tsx
@@ -0,0 +1,25 @@
+"use client";
+import React, { useState } from "react";
+import Image, { ImageProps } from "next/image";
+
+interface ImageWithFallbackProps extends ImageProps {
+  fallbackSrc: string;
+}
+
+const ImageWithFallback = (props: ImageWithFallbackProps) => {
+  const { src, fallbackSrc, ...rest } = props;
+  const [imgSrc, setImgSrc] = useState(src);
+
+  return (
+    <Image
+      {...rest}
+      src={imgSrc}
+      alt={props.alt}
+      onError={() => {
+        setImgSrc(fallbackSrc);
+      }}
+    />
+  );
+};
+
+export default ImageWithFallback;
